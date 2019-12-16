455a8072c1b382bd69422afd5600bb1ac46f57e5
Angelique <angeliquebuijzen@hotmail.com>
2019-12-16 17:52:56 +0100

    Navigatie toegevoegd

--- screens/EndScreen.js
+++ screens/EndScreen.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Button, View, StyleSheet, Text } from "react-native";
 
-export default class PlayersScreen extends React.Component {
+
+export default class EndScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>StartScherm</Text>
-          
+          <Text>Einde</Text>
+          <Button
+            title="Startscherm"
+            onPress={() => this.props.navigation.navigate('PlayersScreen')}
+          />
           <Button
             title="Game"
             onPress={() => this.props.navigation.navigate('PlayScreen')}
           />
-
-            <Button
-            title="einde"
-            onPress={() => this.props.navigation.navigate('EndScreen')}
-            />
         </View>
-
-
-
       );
     }
   }
