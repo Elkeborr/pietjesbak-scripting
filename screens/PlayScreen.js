82a4af0812b2a96d875ee3e92043cc4d475d4fe6
Elkeborr <elkeborreij@student.thomasmore.be>
2019-12-16 17:53:16 +0100

    update playscreen

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -1,2 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, View, StyleSheet, Text } from "react-native";
+
+export default class ContactDetailsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Dice 1</Text>
+        <Button title="Rol" onPress={this.rollDice} />
+      </View>
+    );
+  }
+}
+
+rollDice = () => {};
