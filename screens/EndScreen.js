abb622109dadd4ab1f7eac30b2ad9792a479a331
jonas vanhamme <jonas.vanhamme@hotmail.com>
2020-01-13 17:18:04 +0100

    Result text

--- screens/EndScreen.js
+++ screens/EndScreen.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Button, View, StyleSheet, Text } from "react-native";
 
-export default class App extends React.Component{
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Gefeliciteerd, u heeft gewonnen!</Text>
-                <Button title="Rematch"></Button>
-                <Button title="Home"></Button>
-            </View>
-        );
-    };
+function PlayerWon() {
+  return <Text>Congratulations, you won!</Text>
+}
+
+function PlayerLost() {
+  return <Text>Sorry, you lost!</Text>
+}
+
+function Result(props){
+  const hasWon = props.hasWon;
+  if(hasWon) {
+    return <PlayerWon />;
+  }
+  return <PlayerLost />;
 }
 
 
@@ -18,16 +22,18 @@ export default class EndScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Einde</Text>
+          <Result hasWon={false}/>
           <Button
-            title="Startscherm"
+            title="Home"
             onPress={() => this.props.navigation.navigate('PlayersScreen')}
           />
           <Button
-            title="Game"
-            onPress={() => this.props.navigation.navigate('PlayScreen')}
+            title="Rematch"
+            onPress={() => this.props.navigation.navigate('Playscreen')}
           />
         </View>
       );
     }
   }
+
+  
