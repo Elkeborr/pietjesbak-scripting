84f0193900503398b4124453eee2777b3fdbe6a7
Elkeborr <elkeborreij@student.thomasmore.be>
2020-01-13 22:37:14 +0100

    uitleg

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -13,21 +13,30 @@ import {
 let sideuno;
 let sidedos;
 let sidetres;
+let countPlus;
 export default class PlayScreen extends React.Component {
-
- 
   constructor(props) {
     super(props);
-    this.state = { sideOne: 0, sideTwo: 0, sideTree: 0 };
+    this.state = { sideOne: 0, sideTwo: 0, sideTree: 0, count: 0 };
     this._rollDice = this._rollDice.bind(this);
   }
 
   _rollDice() {
-   sideuno = Math.floor(Math.random() * 6);
-   sidedos = Math.floor(Math.random() * 6);
-   sidetres = Math.floor(Math.random() * 6);
-   points(sideuno, sidedos, sidetres);
-    this.setState({ sideOne: sideuno, sideTwo: sidedos, sideTree: sidetres });
+    // De kant van de dobbelsteen bepalen
+    sideuno = Math.floor(Math.random() * 6);
+    sidedos = Math.floor(Math.random() * 6);
+    sidetres = Math.floor(Math.random() * 6);
+    // Aantal keer je klikt optellen
+    countPlus = this.state.count + 1;
+    // Functie aanroepen waarmee je gaat zien hoeveel u totaal is
+    points(sideuno, sidedos, sidetres);
+    // De state (status) veranderen van de stenen en de kliks
+    this.setState({
+      sideOne: sideuno,
+      sideTwo: sidedos,
+      sideTree: sidetres,
+      count: countPlus
+    });
   }
 
   render() {
@@ -64,25 +73,24 @@ export default class PlayScreen extends React.Component {
   }
 }
 
-
 /* Optellen van de punten en in een array steken */
-function points(sideuno, sidedos, sidetres){
-  let point1 = setScore(sideuno+1);
-  let point2 = setScore(sidedos +1);
-  let point3 = setScore(sidetres +1);
-  let points= point1 + point2 + point3;
-  console.log(points);
+function points(sideuno, sidedos, sidetres) {
+  let point1 = setScore(sideuno + 1);
+  let point2 = setScore(sidedos + 1);
+  let point3 = setScore(sidetres + 1);
+  let points = point1 + point2 + point3;
+  //console.log(points);
 }
 
 /* Wat zijn u stenen waard, 1=100, 6==60, de rest is het zelfde */
-function setScore(score){
+function setScore(score) {
   switch (score) {
     case 1:
-        score = 100;
-        break;
+      score = 100;
+      break;
     case 6:
-        score = 60;
-        break;
-}
-return  score;
+      score = 60;
+      break;
+  }
+  return score;
 }
