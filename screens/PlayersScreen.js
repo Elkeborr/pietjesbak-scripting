82e636d8a2dfb0e8b35cc7aeb66baef51003d82c
jonas vanhamme <jonas.vanhamme@hotmail.com>
2020-01-15 05:14:22 +0100

    Player name input

--- screens/PlayersScreen.js
+++ screens/PlayersScreen.js
@@ -1,25 +1,74 @@
-import React from "react";
-import { Button, View, StyleSheet, Text } from "react-native";
+import React, { Component, useState } from "react";
+import {View, Text, Button, TextInput, FlatList} from "react-native";
+/*
+import Player from './components/player/player';
+import AddPlayer from './components/player/addPlayer';
+*/
+export default function addPlayers() {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
 
-export default class PlayersScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>StartScherm</Text>
-          
-          <Button
-            title="Game"
-            onPress={() => this.props.navigation.navigate('PlayScreen')}
-          />
-
-            <Button
-            title="einde"
-            onPress={() => this.props.navigation.navigate('EndScreen')}
-            />
-        </View>
-
-
-
-      );
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText); // set de ingevulde naam
     }
-  }
+
+    const addGoalHandler = () => {
+        setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    }
+    
+    return (
+        <View>
+        <View>
+            <TextInput placeholder="Voeg speler toe" onChangeText={goalInputHandler} value={enteredGoal} />
+            <Button title="voeg toe" onPress={addGoalHandler} />
+        </View>
+        <View>
+            {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        </View>
+        </View>
+    );
+}
+
+
+
+
+
+
+
+
+/*
+
+
+    const [enteredPlayers, setEnteredPlayers] = useState([]);
+
+    const addPlayer = enteredName => {
+        console.log(enteredName);
+        setEnteredPlayers(currentPlayers => [...currentPlayers, {id: Math.random().toString(), name: enteredName}]); // maak array met ingevulde namen
+    }
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>StartScherm</Text>
+      
+      <Button
+        title="Game"
+        onPress={() => this.props.navigation.navigate('PlayScreen')}
+      />
+
+        <Button
+        title="einde"
+        onPress={() => this.props.navigation.navigate('EndScreen')}
+        />
+
+    <View>
+    
+    </View>
+    <AddPlayer onAddPlayer={addPlayer} />
+    <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={enteredPlayers}
+        renderItem={itemData => <Player name={itemData.item.value} />} 
+    />
+    
+    </View>
+  );*/
